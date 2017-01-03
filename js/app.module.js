d12dad0ca9898aca3ad37b7d51c6fcd5920053a5
ward-spacecase <ward.spacecase@gmail.com>
2017-01-03 10:02:25 -0700

    added empty component for dashboard

--- js/app.module.js
+++ js/app.module.js
@@ -1,7 +1,7 @@
 (function() {
 
             angular.module('todoApp', [
-                'welcome', 'storage'
+                'welcome', 'storage', 'dashboard'
             ]);
 
 
---
+++ js/components/dashboard/dashboard.component.js
@@ -0,0 +1,14 @@
+(function() {
+    angular
+        .module('dashboard')
+            .component('dashboard', {
+                templateUrl: 'js/components/dashboard/dashboard.template.html'
+            })
+        .controller('DashController', function() {
+            var vm = this;
+
+
+
+        });
+
+})();
